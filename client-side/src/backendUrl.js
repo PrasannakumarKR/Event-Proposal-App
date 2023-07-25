e871e98f0db6ae9c5b28656c8866a5ab668b184a
Prasannakumar K R <prasannakumarkr04@example.com>
2023-07-25 15:34:13 +0530

    images are added

--- client-side/src/backendUrl.js
+++ client-side/src/backendUrl.js
@@ -1,7 +1,2 @@
-const url = 'https://event-proposal-server.onrender.com'
-<<<<<<< HEAD
-
-export default url
-=======
-export default url
->>>>>>> 1a3fd5047eb193ace7fc2c710bbb9a6473c9e346
+const url = 'https://event-proposal-server.onrender.com';
+export default url;
